f48dca3393c95419657de4e23024ed723a9f18f3
SarapaevRoman <146869487+SarapaevRoman@users.noreply.github.com>
2025-05-17 22:30:54 +0300

    Update plot_utils.py

--- plot_utils.py
+++ plot_utils.py
@@ -1,27 +1,3 @@
-'''
-from matplotlib import pyplot as plt
-import io
-
-def plot_forecast(dates, prices, predicted_date, predicted_price):
-    # Генерируем график
-    plt.figure(figsize=(10, 5))
-    plt.plot(dates, prices, label="Историческая цена")
-    plt.scatter(predicted_date, predicted_price, color='red', label="Прогноз", zorder=5)
-    plt.title("Прогноз цены")
-    plt.xlabel("Дата")
-    plt.ylabel("Цена")
-    plt.legend()
-    plt.grid(True)
-    plt.tight_layout()
-
-    # Сохраняем график в память (в байтовый буфер)
-    buf = io.BytesIO()
-    plt.savefig(buf, format='png')
-    buf.seek(0)
-    plt.close()
-
-    return buf  # Возвращаем байтовый поток (BytesIO), а не PIL.Image
-'''
 from matplotlib import pyplot as plt
 import io
 import pandas as pd
